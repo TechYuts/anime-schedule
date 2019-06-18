eebf8006bd84f98bef23e9218b3bf9a020b8ae6f
yuta yanagisawa <yuta.yanagisawa@soliton.co.jp>
2019-06-18 18:30:01 +0900

    edit card detail

--- src/components/layout/CardDetail.tsx
+++ src/components/layout/CardDetail.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Card from '@material-ui/core/Card';
-import { withStyles, Theme } from '@material-ui/core/styles';
+import { makeStyles, Theme } from '@material-ui/core/styles';
 import { display, fontSize } from '@material-ui/system';
 
 interface AnimeCardProperties {
@@ -11,50 +11,46 @@ interface AnimeCardProperties {
   id: number;
 }
 
-export default class CardDetail extends Component<AnimeCardProperties, { flipped: boolean }> {
-  constructor(props: AnimeCardProperties) {
-    super(props);
-  }
+const styles = (theme: Theme): object => ({
+  card: {
+    display: 'flex',
+    margin: 10,
+  },
+  title: {
+    fontSize: 15,
+  },
+  details: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  content: {
+    flex: '1 0 auto',
+    height: 60,
+    width: 160,
+  },
+  cover: {
+    width: 160,
+    height: 224,
+  },
+  genres: {
+    display: 'flex',
+    alignItems: 'center',
+    paddingLeft: theme.spacing(1),
+    paddingBottom: theme.spacing(1),
+  },
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+  },
+  chip: {
+    margin: 1,
+    fontSize: 10,
+  },
+});
 
-  styles = (theme: Theme) => ({
-    card: {
-      display: 'flex',
-      margin: 10,
-    },
-    title: {
-      fontSize: 15,
-    },
-    details: {
-      display: 'flex',
-      flexDirection: 'column',
-    },
-    content: {
-      flex: '1 0 auto',
-      height: 60,
-      width: 160,
-    },
-    cover: {
-      width: 160,
-      height: 224,
-    },
-    genres: {
-      display: 'flex',
-      alignItems: 'center',
-      paddingLeft: theme.spacing(1),
-      paddingBottom: theme.spacing(1),
-    },
-    root: {
-      display: 'flex',
-      justifyContent: 'center',
-      flexWrap: 'wrap',
-    },
-    chip: {
-      margin: 1,
-      fontSize: 10,
-    },
-  });
+export default function CardDetail(props: AnimeCardProperties): JSX.Element {
+  const classes = makeStyles(styles);
 
-  render(): JSX.Element {
-    return <div></div>;
-  }
+  return <Card key={props.id}> </Card>;
 }
